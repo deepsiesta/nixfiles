9ccd48ae5e56cd47355634da54da8fefaf511851
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-08-29 17:49:12 +0200

    Refactor fastfetch config

--- modules/fastfetch/default.nix
+++ modules/fastfetch/default.nix
@@ -3,6 +3,96 @@
     programs.fastfetch = {
       enable = true;
     };
-    home.file.".config/fastfetch/config.jsonc".source = ./config.jsonc;
+    home.file.".config/fastfetch/config.jsonc".text = let
+      ascii_logo =
+        builtins.replaceStrings ["\n"] ["\\n"]
+        (builtins.readFile ./logo.txt);
+    in
+      # JSONC
+      ''
+        {
+          "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
+          "logo": {
+            "type": "data",
+            "source": "${ascii_logo}",
+            "color": {"1":"default"},
+            "padding": {
+              "top": 1,
+              "right": 4
+            }
+          },
+          "display": {
+            "separator": " "
+          },
+          "modules": [
+            "break",
+            "break",
+            {
+              "type": "title",
+              "keyWidth": 10
+            },
+            "break",
+            {
+              "type": "os",
+              "format": "{3}",
+              "key": " ",
+              "keyColor": "36" // = color4
+            },
+            {
+              "type": "kernel",
+              "key": " ",
+              "keyColor": "36"
+            },
+            {
+              "type": "cpu",
+              "format": "{1}",
+              "key": " ",
+              "keyColor": "36"
+            },
+            {
+              "type": "gpu",
+              "format": "{1} {2}",
+              "key": "󰢮 ",
+              "keyColor": "36"
+            },
+            {
+              "type": "packages",
+              // "format": "{} (nix)",
+              "key": " ",
+              "keyColor": "36"
+            },
+            {
+              "type": "shell",
+              "key": " ",
+              "keyColor": "36"
+            },
+            {
+              "type": "terminal",
+              "key": " ",
+              "keyColor": "36"
+            },
+            {
+              "type": "wm",
+              "key": " ",
+              "keyColor": "36"
+            },
+            {
+              "type": "theme",
+              "key": " ",
+              "keyColor": "36"
+            },
+            {
+              "type": "terminalfont",
+              //           "type": "font",
+              "key": " ",
+              "keyColor": "36"
+            },
+            "break",
+            "colors",
+            "break",
+            "break"
+          ]
+        }
+      '';
   };
 }
