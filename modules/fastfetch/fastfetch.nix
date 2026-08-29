4933fd2d1e8876a9eb43a388fc09eb4399edf006
Siesta <20047950+deepsiesta@users.noreply.github.com>
2026-08-29 19:41:18 +0200

    Update fastfetch config

--- modules/fastfetch/fastfetch.nix
+++ modules/fastfetch/fastfetch.nix
@@ -26,7 +26,6 @@
           },
           "modules": [
             "break",
-            "break",
             {
               "type": "title",
               "keyWidth": 10
@@ -61,10 +60,18 @@
               "key": " ",
               "keyColor": "36"
             },
+            "break",
             {
-              "type": "shell",
-              "key": " ",
+              "type": "wm",
+              "key": " ",
               "keyColor": "36"
+            },
+            {
+              // "type": "theme",
+              "type": "command",
+              "key": " ",
+              "keyColor": "36",
+              "text": "awk -F'\"' '/^(builtin|community_palette|custom_palette) =/ {print $2; exit}' ~/.config/noctalia/config.toml"
             },
             {
               "type": "terminal",
@@ -72,20 +79,27 @@
               "keyColor": "36"
             },
             {
-              "type": "wm",
-              "key": " ",
+              "type": "shell",
+              "key": " ",
               "keyColor": "36"
             },
             {
-              "type": "theme",
-              "key": " ",
-              "keyColor": "36"
-            },
-            {
-              "type": "terminalfont",
-              //           "type": "font",
+              // "type": "terminalfont",
+              "type": "command",
               "key": " ",
-              "keyColor": "36"
+              "keyColor": "36",
+              "text": "font=$(awk '/^font_family/ {$1=\"\"; sub(/^ /, \"\"); print}' ~/.config/kitty/kitty.conf | sed 's/\\\\([a-z]\\\\)\\\\([A-Z]\\\\)/\\\\1 \\\\2/g'); size=$(awk '/^font_size/ {print $2}' ~/.config/kitty/kitty.conf); echo \"$font (''${size}pt)\""
+            },
+            "break",
+            {
+              "type": "command",
+              "key": "󰔠 ",
+              "text": "f() { [ $1 -gt 0 ] && echo \"$1 $2$([ $1 -ne 1 ] && echo s) \"; }; b=$(stat -c %W / 2>/dev/null); [ \"$b\" = \"0\" ] && b=$(stat -c %Y /); d=$(($(date +%s) - b)); y=$((d / 31536000)); rem=$((d % 31536000)); m=$((rem / 2592000)); day=$(((rem % 2592000) / 86400)); out=\"$(f $y year)$(f $m month)$(f $day day)\"; echo \"''${out:-Today}\""
+            },
+            {
+              "type": "command",
+              "key": "󰚰 ",
+              "text": "echo \"Generation $(readlink /nix/var/nix/profiles/system | cut -d'-' -f2)\""
             },
             "break",
             "colors",
